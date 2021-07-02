77e1f50e8d77a8cd1bb577bf4992643b2285a0f8
YL <simon-ling@hotmail.com>
2021-07-02 19:58:40 -0400

    t4 is done

--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,24 @@
-// TypeScript doesn't allow you to redefine variable type
-// Once type is define, it can't be alerted to another type
-let character = "mario";
-let age = 30;
-let isBlackBelt = false;
-
-character = "luigi";
-age = 40;
-isBlackBelt = true;
-const circ = (diam: number) => {
-  return diam * Math.PI;
+// array
+let names = ["luigi", "mario", "yoshi"];
+names.push("toad"); // only string can be pushed
+names[0] = "dave";
+let nums = [1, 2, 3, 4];
+nums.push(-1);
+nums[0] = -1;
+let mixes = ["ken", 4, "chun-li", -9, true];
+mixes.push("ryu");
+mixes.push(10);
+mixes.push(false);
+mixes[0] = 11;
+// object
+let ninja = {
+  name: "mario",
+  belt: "black",
+  age: 30,
 };
-console.log(circ(2.5));
+// once obj is created, we can't add properties
+ninja = {
+  name: "ryu",
+  belt: "white",
+  age: 20,
+};
