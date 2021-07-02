2cdd5d1dbc7ec1b3867a2b8abf1875241a925036
YL <simon-ling@hotmail.com>
2021-07-01 22:59:44 -0400

    t2 is done

---
+++ sandbox.ts
@@ -0,0 +1,8 @@
+// const character='mario'
+const character='luigi'
+console.log(character)
+const inputs=document.querySelectorAll('input')
+console.log(inputs)
+inputs.forEach(input=>{
+    console.log(input)
+})
