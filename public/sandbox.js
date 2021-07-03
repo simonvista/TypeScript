b96bd22d7919dd45c8776b91802262437cdb315b
YL <simon-ling@hotmail.com>
2021-07-02 23:45:18 -0400

    t9-0110 is done

--- public/sandbox.js
+++ public/sandbox.js
@@ -5,3 +5,10 @@ var logDetail = function (id, item) {
 var greet = function (user) {
     console.log(user.name + " id: " + user.id);
 };
+var greet1 = function (user) {
+    console.log(user.name + " id is: " + user.id);
+};
+logDetail(101, "Amy");
+var user = { id: 11, name: "ruy" };
+greet(user);
+greet1(user);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,6 +1,15 @@
-const logDetail = (id: string | number, item: string) => {
+type strOrNum = string | number;
+type nameIdObj = { id: strOrNum; name: string };
+const logDetail = (id: strOrNum, item: string) => {
   console.log(`${item} id: ${id}`);
 };
-const greet = (user: { id: string | number; name: string }) => {
+const greet = (user: nameIdObj) => {
   console.log(`${user.name} id: ${user.id}`);
 };
+const greet1 = (user: nameIdObj) => {
+  console.log(`${user.name} id is: ${user.id}`);
+};
+logDetail(101, "Amy");
+let user = { id: 11, name: "ruy" };
+greet(user);
+greet1(user);
