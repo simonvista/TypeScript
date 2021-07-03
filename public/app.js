4f8fcaa2f1cd190ac3b645cef582a2777557ac2e
YL <simon-ling@hotmail.com>
2021-07-03 10:23:15 -0400

    t11-0445 is done

--- public/app.js
+++ public/app.js
@@ -3,4 +3,11 @@
 /* const anchor = document.querySelector("a")!;
 console.log(anchor);
 console.log(anchor.href); */
-var form = document.querySelector("form");
+// const form = document.querySelector("form")!;
+// type casing by as key word
+// const form: HTMLFormElement
+var form = document.querySelector(".new-item-form");
+console.log(form.children);
+// input
+var type = document.querySelector(".field>label");
+console.log(type === null || type === void 0 ? void 0 : type.textContent);
--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,11 @@
 /* const anchor = document.querySelector("a")!;
 console.log(anchor);
 console.log(anchor.href); */
-const form = document.querySelector("form")!;
+// const form = document.querySelector("form")!;
+// type casing by as key word
+// const form: HTMLFormElement
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+console.log(form.children);
+// input
+const type = document.querySelector(".field>label");
+console.log(type?.textContent);
