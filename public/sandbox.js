6ba7bf00b9aa2dbd07e7b7a7fa31059d33e3335c
YL <simon-ling@hotmail.com>
2021-07-02 22:37:23 -0400

    t8-0000 is done

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("hi ninjas");
+/* let greet = () => {
+  console.log("hi");
+};
+greet()*/
+var greet;
+greet = function () {
+    console.log("hi again");
+};
+greet();
+var add = function (a, b, c) {
+    console.log(a + b);
+    console.log(c);
+};
+add(3, 1.2);
+add(3, 1.2, 1.23);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,15 @@
-console.log("hi ninjas");
+/* let greet = () => {
+  console.log("hi");
+}; 
+greet()*/
+let greet: Function;
+greet = () => {
+  console.log("hi again");
+};
+greet();
+const add = (a: number, b: number, c?: number | string) => {
+  console.log(a + b);
+  console.log(c);
+};
+add(3, 1.2);
+add(3, 1.2, 1.23);
