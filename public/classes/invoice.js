1e95a316254b7b6c41af7d1426f74f9ca9465cbd
YL <simon-ling@hotmail.com>
2021-07-03 19:30:22 -0400

    t16-0000 is done

--- public/classes/invoice.js
+++ public/classes/invoice.js
@@ -5,6 +5,7 @@ export class Invoice {
         this.details = details;
         this.amount = amount;
     }
+    // implements interface
     format() {
         return `${this.client} owes $${this.amount} for ${this.details}, `;
     }
--- src/classes/Invoice.ts
+++ src/classes/Invoice.ts
@@ -1,10 +1,12 @@
+import { HasFormatter } from "../interfaces/HasFormatter";
 // class
-export class Invoice {
+export class Invoice implements HasFormatter {
   constructor(
     readonly client: string,
     private details: string,
     public amount: number
   ) {}
+  // implements interface
   format() {
     return `${this.client} owes $${this.amount} for ${this.details}, `;
   }
