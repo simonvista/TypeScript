592bd69d5e78eb7efb66528d49e53f00b6546b1e
YL <simon-ling@hotmail.com>
2021-07-03 10:58:42 -0400

    t12-0000 is done

--- public/app.js
+++ public/app.js
@@ -1,25 +1,17 @@
 "use strict";
-// const anchor: HTMLAnchorElement
-/* const anchor = document.querySelector("a")!;
-console.log(anchor);
-console.log(anchor.href); */
-// const form = document.querySelector("form")!;
-// type casing by as key word
-// const form: HTMLFormElement
-var form = document.querySelector(".new-item-form");
-// console.log(form.children);
-// input
-// const type = document.querySelector(".field>label");
-// console.log(type?.textContent);
-var select = document.querySelector("#type");
-// console.log(select);
-var input1 = document.querySelector("#tofrom");
-// console.log(input1);
-var input2 = document.querySelector("#details");
-// console.log(input2);
-var input3 = document.querySelector("#amount");
-// console.log(input3);
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    console.log(select.value, input1.value, input2.value, input3.valueAsNumber);
-});
+// class
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details + ", ";
+    };
+    return Invoice;
+}());
+var invoice1 = new Invoice("Luigi", "tuition", 121);
+console.log(invoice1.format());
+var invoice2 = new Invoice("Mario", "Mario Website Design", 221);
+console.log(invoice2.format());
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,18 @@
-// const anchor: HTMLAnchorElement
-/* const anchor = document.querySelector("a")!;
-console.log(anchor);
-console.log(anchor.href); */
-// const form = document.querySelector("form")!;
-// type casing by as key word
-// const form: HTMLFormElement
-const form = document.querySelector(".new-item-form") as HTMLFormElement;
-// console.log(form.children);
-// input
-// const type = document.querySelector(".field>label");
-// console.log(type?.textContent);
-const select = document.querySelector("#type") as HTMLSelectElement;
-// console.log(select);
-const input1 = document.querySelector("#tofrom") as HTMLInputElement;
-// console.log(input1);
-const input2 = document.querySelector("#details") as HTMLInputElement;
-// console.log(input2);
-const input3 = document.querySelector("#amount") as HTMLInputElement;
-// console.log(input3);
-form.addEventListener("submit", (e: Event) => {
-  e.preventDefault();
-  console.log(select.value, input1.value, input2.value, input3.valueAsNumber);
-});
+// class
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}, `;
+  }
+}
+const invoice1 = new Invoice("Luigi", "tuition", 121);
+console.log(invoice1.format());
+const invoice2 = new Invoice("Mario", "Mario Website Design", 221);
+console.log(invoice2.format());
