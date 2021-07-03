5f8bd97d73f95ae5220aa7aa3d3db36be9e57573
YL <simon-ling@hotmail.com>
2021-07-02 22:59:15 -0400

    t8-0640 is done

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -8,14 +8,14 @@ greet = () => {
 };
 greet();
 // function w/o return
-const add = (a: number, b: number, c: number | string = 2) => {
+const add = (a: number, b: number, c: number | string = 2): void => {
   console.log(a + b);
   console.log(c);
 };
 add(3, 1.2);
 add(3, 1.2, 1.23);
 // function w/o return
-const minus = (a: number, b: number) => {
+const minus = (a: number, b: number): number => {
   return a - b;
 };
 let res = minus(1, 2.3);
