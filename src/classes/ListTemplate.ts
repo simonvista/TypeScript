10773a39489e0ca2b527741731857d1a4493f484
YL <simon-ling@hotmail.com>
2021-07-03 21:25:17 -0400

    t17-0000 is done

---
+++ src/classes/ListTemplate.ts
@@ -0,0 +1,26 @@
+import { HasFormatter } from "../interfaces/HasFormatter.js";
+
+/* 
+    1. register a list container (ul) in the comstructor
+    2. create a render method to render a new 'li' to the container
+        -- accepts arguments: invoice or payment, a heading, a position
+        -- create html template (li,h4,p)
+        -- add the 'li' template to the start/end of the list
+*/
+export class ListTemplate {
+  constructor(private container: HTMLUListElement) {}
+  render(item: HasFormatter, heading: string, pos: "start" | "end") {
+    const li = document.createElement("li");
+    const h4 = document.createElement("h4");
+    h4.innerText = heading;
+    li.append(h4);
+    const p = document.createElement("p");
+    p.innerText = item.format();
+    li.append(p);
+    if (pos === "start") {
+      this.container.prepend(li);
+    } else {
+      this.container.append(li);
+    }
+  }
+}
